38beb4123e2e51b5732f2b2d9aea71d6dc8cf510
Chad Darby <darby@luv2code.com>
2023-04-27 18:06:39 -0400

    spring mvc security - batch 2

---
+++ 08-spring-boot-spring-mvc-security/09-spring-boot-spring-mvc-security-custom-access-denied-page/src/main/java/com/luv2code/springboot/demosecurity/controller/DemoController.java
@@ -0,0 +1,40 @@
+package com.luv2code.springboot.demosecurity.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class DemoController {
+
+    @GetMapping("/")
+    public String showHome() {
+
+        return "home";
+    }
+
+    // add a request mapping for /leaders
+
+    @GetMapping("/leaders")
+    public String showLeaders() {
+
+        return "leaders";
+    }
+
+    // add request mapping for /systems
+
+    @GetMapping("/systems")
+    public String showSystems() {
+
+        return "systems";
+    }
+
+}
+
+
+
+
+
+
+
+
+
