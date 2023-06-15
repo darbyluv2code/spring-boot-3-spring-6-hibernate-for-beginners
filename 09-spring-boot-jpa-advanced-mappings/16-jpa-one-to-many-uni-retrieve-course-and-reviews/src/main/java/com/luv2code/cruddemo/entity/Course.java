51850a2c7e06f22f176cc98c0a98a67e4a5c7ead
Chad Darby <darby@luv2code.com>
2023-06-14 21:49:44 -0400

    jpa advanced mappings - OneToMany - batch 3

---
+++ 09-spring-boot-jpa-advanced-mappings/16-jpa-one-to-many-uni-retrieve-course-and-reviews/src/main/java/com/luv2code/cruddemo/entity/Course.java
@@ -0,0 +1,103 @@
+package com.luv2code.cruddemo.entity;
+
+import jakarta.persistence.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name="course")
+public class Course {
+
+    // define our fields
+
+    // define constructors
+
+    // define getter setters
+
+    // define toString
+
+    // annotate fields
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="id")
+    private int id;
+
+    @Column(name="title")
+    private String title;
+
+    @ManyToOne(cascade = {CascadeType.PERSIST, CascadeType.MERGE,
+                          CascadeType.DETACH, CascadeType.REFRESH})
+    @JoinColumn(name="instructor_id")
+    private Instructor instructor;
+
+    @OneToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    @JoinColumn(name = "course_id")
+    private List<Review> reviews;
+
+    public Course() {
+
+    }
+
+    public Course(String title) {
+        this.title = title;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Instructor getInstructor() {
+        return instructor;
+    }
+
+    public void setInstructor(Instructor instructor) {
+        this.instructor = instructor;
+    }
+
+    public List<Review> getReviews() {
+        return reviews;
+    }
+
+    public void setReviews(List<Review> reviews) {
+        this.reviews = reviews;
+    }
+
+    // add a convenience method
+
+    public void addReview(Review theReview) {
+
+        if (reviews == null) {
+            reviews = new ArrayList<>();
+        }
+
+        reviews.add(theReview);
+    }
+
+    @Override
+    public String toString() {
+        return "Course{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                '}';
+    }
+}
+
+
+
+
+
+
---
+++ 09-spring-boot-jpa-advanced-mappings/16-jpa-one-to-many-uni-retrieve-course-and-reviews/src/test/java/com/luv2code/cruddemo/CruddemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luv2code.cruddemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CruddemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
