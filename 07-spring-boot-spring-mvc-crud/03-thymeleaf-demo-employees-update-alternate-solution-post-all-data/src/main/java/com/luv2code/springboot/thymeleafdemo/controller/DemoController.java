17a05aa5844c485f54611b56fb4edd5817d05192
Chad Darby <darby@luv2code.com>
2023-06-16 07:56:28 -0400

    spring-mvc-crud alternate solution to post all data

---
+++ 07-spring-boot-spring-mvc-crud/03-thymeleaf-demo-employees-update-alternate-solution-post-all-data/src/main/java/com/luv2code/springboot/thymeleafdemo/controller/DemoController.java
@@ -0,0 +1,27 @@
+package com.luv2code.springboot.thymeleafdemo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class DemoController {
+
+	// create a mapping for "/hello"
+	
+	@GetMapping("/hello")
+	public String sayHello(Model theModel) {
+		
+		theModel.addAttribute("theDate", new java.util.Date());
+		
+		return "helloworld";
+	}
+}
+
+
+
+
+
+
+
+
---
+++ 07-spring-boot-spring-mvc-crud/03-thymeleaf-demo-employees-update-alternate-solution-post-all-data/src/main/java/com/luv2code/springboot/thymeleafdemo/service/EmployeeService.java
@@ -0,0 +1,17 @@
+package com.luv2code.springboot.thymeleafdemo.service;
+
+import java.util.List;
+
+import com.luv2code.springboot.thymeleafdemo.entity.Employee;
+
+public interface EmployeeService {
+
+	List<Employee> findAll();
+	
+	Employee findById(int theId);
+	
+	void save(Employee theEmployee);
+	
+	void deleteById(int theId);
+	
+}
