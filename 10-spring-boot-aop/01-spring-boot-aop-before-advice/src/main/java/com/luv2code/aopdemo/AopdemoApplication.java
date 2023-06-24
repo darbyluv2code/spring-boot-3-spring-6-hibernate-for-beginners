583e32a947c4a28f6e4f78b7ea40485229a987d3
Chad Darby <darby@luv2code.com>
2023-06-24 17:23:42 -0400

    AOP - batch 1

---
+++ 10-spring-boot-aop/01-spring-boot-aop-before-advice/src/main/java/com/luv2code/aopdemo/AopdemoApplication.java
@@ -0,0 +1,37 @@
+package com.luv2code.aopdemo;
+
+import com.luv2code.aopdemo.dao.AccountDAO;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+@SpringBootApplication
+public class AopdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AopdemoApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner commandLineRunner(AccountDAO theAccountDAO) {
+
+		return runner -> {
+
+			demoTheBeforeAdvice(theAccountDAO);
+		};
+	}
+
+	private void demoTheBeforeAdvice(AccountDAO theAccountDAO) {
+
+		// call the business method
+		theAccountDAO.addAccount();
+
+		// do it again!
+		System.out.println("\nlet's call it again!\n");
+
+		// call the business method again
+		theAccountDAO.addAccount();
+	}
+}
---
+++ 10-spring-boot-aop/01-spring-boot-aop-before-advice/src/main/java/com/luv2code/aopdemo/dao/AccountDAO.java
@@ -0,0 +1,7 @@
+package com.luv2code.aopdemo.dao;
+
+public interface AccountDAO {
+
+    void addAccount();
+
+}
