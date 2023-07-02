295caef7f9504c008bd1dd672c853e9d99eee5d6
Chad Darby <darby@luv2code.com>
2023-07-02 11:14:35 -0400

    AOP - batch 2

---
+++ 10-spring-boot-aop/14-spring-boot-aop-demo-aspect-order/src/main/java/com/luv2code/aopdemo/aspect/MyDemoLoggingAspect.java
@@ -0,0 +1,29 @@
+package com.luv2code.aopdemo.aspect;
+
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+@Order(2)
+public class MyDemoLoggingAspect {
+
+    @Before("com.luv2code.aopdemo.aspect.LuvAopExpressions.forDaoPackageNoGetterSetter()")
+    public void beforeAddAccountAdvice() {
+        System.out.println("\n=====>>> Executing @Before advice on method");
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
