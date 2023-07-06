8ce82e3a99d37ed6a8b9d0f9406be144ad4a8528
Chad Darby <darby@luv2code.com>
2023-07-05 20:41:27 -0400

    AOP - batch 2

---
+++ 10-spring-boot-aop/19-spring-boot-aop-demo-after-advice/src/main/java/com/luv2code/aopdemo/aspect/MyCloudLogAsyncAspect.java
@@ -0,0 +1,18 @@
+package com.luv2code.aopdemo.aspect;
+
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+@Order(1)
+public class MyCloudLogAsyncAspect {
+
+    @Before("com.luv2code.aopdemo.aspect.LuvAopExpressions.forDaoPackageNoGetterSetter()")
+    public void logToCloudAsync() {
+        System.out.println("\n=====>>> Logging to Cloud in async fashion");
+    }
+
+}
---
+++ 10-spring-boot-aop/19-spring-boot-aop-demo-after-advice/src/main/java/com/luv2code/aopdemo/aspect/MyDemoLoggingAspect.java
@@ -0,0 +1,116 @@
+package com.luv2code.aopdemo.aspect;
+
+import com.luv2code.aopdemo.Account;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.*;
+import org.aspectj.lang.reflect.MethodSignature;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Aspect
+@Component
+@Order(2)
+public class MyDemoLoggingAspect {
+
+    @After("execution(* com.luv2code.aopdemo.dao.AccountDAO.findAccounts(..))")
+    public void afterFinallyFindAccountsAdvice(JoinPoint theJoinPoint) {
+
+        // print out which method we are advising on
+        String method = theJoinPoint.getSignature().toShortString();
+        System.out.println("\n=====>>> Executing @After (finally) on method: " + method);
+    }
+
+
+    @AfterThrowing(
+            pointcut = "execution(* com.luv2code.aopdemo.dao.AccountDAO.findAccounts(..))",
+            throwing = "theExc")
+    public void afterThrowingFindAccountsAdvice(
+            JoinPoint theJoinPoint, Throwable theExc) {
+
+        // print out which method we are advising on
+        String method = theJoinPoint.getSignature().toShortString();
+        System.out.println("\n=====>>> Executing @AfterThrowing on method: " + method);
+
+        // log the exception
+        System.out.println("\n=====>>> The exception is: " + theExc);
+    }
+
+    @AfterReturning(
+            pointcut = "execution(* com.luv2code.aopdemo.dao.AccountDAO.findAccounts(..))",
+            returning = "result")
+    public void afterReturningFindAccountsAdvice(JoinPoint theJoinPoint, List<Account> result) {
+
+        // print out which method we are advising on
+        String method = theJoinPoint.getSignature().toShortString();
+        System.out.println("\n=====>>> Executing @AfterReturning on method: " + method);
+
+        // print out the results of the method call
+        System.out.println("\n=====>>> result is: " + result);
+
+        // let's post-process the data ... let's modify it :-)
+
+        // convert the account names to uppercase
+        convertAccountNamesToUpperCase(result);
+
+        System.out.println("\n=====>>> result is: " + result);
+
+    }
+
+    private void convertAccountNamesToUpperCase(List<Account> result) {
+
+        // loop through accounts
+
+        for (Account tempAccount : result) {
+
+            // get uppercase version of name
+            String theUpperName = tempAccount.getName().toUpperCase();
+
+            // update the name on the account
+            tempAccount.setName(theUpperName);
+        }
+    }
+
+
+    @Before("com.luv2code.aopdemo.aspect.LuvAopExpressions.forDaoPackageNoGetterSetter()")
+    public void beforeAddAccountAdvice(JoinPoint theJoinPoint) {
+        System.out.println("\n=====>>> Executing @Before advice on method");
+
+        // display the method signature
+        MethodSignature methodSignature = (MethodSignature) theJoinPoint.getSignature();
+
+        System.out.println("Method: " + methodSignature);
+
+        // display method arguments
+
+        // get args
+        Object[] args = theJoinPoint.getArgs();
+
+        // loop thru args
+        for (Object tempArg : args) {
+            System.out.println(tempArg);
+
+            if (tempArg instanceof Account) {
+
+                // downcast and print Account specific stuff
+                Account theAccount = (Account) tempArg;
+
+                System.out.println("account name: " + theAccount.getName());
+                System.out.println("account level: " + theAccount.getLevel());
+            }
+        }
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
