fb5e955fdf4a5e8d32c7e7c5e5cc8c492379b520
Chad Darby <darby@luv2code.com>
2023-07-06 20:54:34 -0400

    AOP - batch 2

---
+++ 10-spring-boot-aop/20-spring-boot-aop-demo-around-advice/src/main/java/com/luv2code/aopdemo/dao/MembershipDAO.java
@@ -0,0 +1,8 @@
+package com.luv2code.aopdemo.dao;
+
+public interface MembershipDAO {
+
+    boolean addSillyMember();
+
+    void goToSleep();
+}
---
+++ 10-spring-boot-aop/20-spring-boot-aop-demo-around-advice/src/main/java/com/luv2code/aopdemo/service/TrafficFortuneService.java
@@ -0,0 +1,7 @@
+package com.luv2code.aopdemo.service;
+
+public interface TrafficFortuneService {
+
+    String getFortune();
+
+}
