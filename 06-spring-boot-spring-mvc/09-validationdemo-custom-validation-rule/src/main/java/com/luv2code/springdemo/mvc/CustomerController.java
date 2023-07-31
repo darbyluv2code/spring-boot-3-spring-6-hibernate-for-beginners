6487180a647bb2f4a5de834d13d3bd69318e3163
Chad Darby <darby@luv2code.com>
2023-07-31 01:43:26 -0400

    Spring MVC Validation - batch final

---
+++ 06-spring-boot-spring-mvc/09-validationdemo-custom-validation-rule/src/main/java/com/luv2code/springdemo/mvc/CustomerController.java
@@ -0,0 +1,61 @@
+package com.luv2code.springdemo.mvc;
+
+import jakarta.validation.Valid;
+import org.springframework.beans.propertyeditors.StringTrimmerEditor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class CustomerController {
+
+    // add an initbinder ... to convert trim input strings
+    // remove leading and trailing whitespace
+    // resolve issue for our validation
+    @InitBinder
+    public void initBinder(WebDataBinder dataBinder) {
+
+        StringTrimmerEditor stringTrimmerEditor = new StringTrimmerEditor(true);
+
+        dataBinder.registerCustomEditor(String.class, stringTrimmerEditor);
+    }
+
+    @GetMapping("/")
+    public String showForm(Model theModel) {
+
+        theModel.addAttribute("customer", new Customer());
+
+        return "customer-form";
+    }
+
+    @PostMapping("/processForm")
+    public String processForm(
+            @Valid @ModelAttribute("customer") Customer theCustomer,
+            BindingResult theBindingResult) {
+
+        System.out.println("Last name: |" + theCustomer.getLastName() + "|");
+
+        System.out.println("Binding results: " + theBindingResult.toString());
+
+        System.out.println("\n\n\n\n");
+
+        if (theBindingResult.hasErrors()) {
+            return "customer-form";
+        }
+        else {
+            return "customer-confirmation";
+        }
+    }
+}
+
+
+
+
+
+
+
---
+++ 06-spring-boot-spring-mvc/09-validationdemo-custom-validation-rule/src/main/java/com/luv2code/springdemo/mvc/validation/CourseCode.java
@@ -0,0 +1,39 @@
+package com.luv2code.springdemo.mvc.validation;
+
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Constraint(validatedBy = CourseCodeConstraintValidator.class)
+@Target({ElementType.METHOD, ElementType.FIELD})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface CourseCode {
+
+    // define default course code
+    public String value() default "LUV";
+
+    // define default error message
+    public String message() default "must start with LUV";
+
+    // define default groups
+    public Class<?>[] groups() default {};
+
+    // define default payloads
+    public Class<? extends Payload>[] payload() default {};
+}
+
+
+
+
+
+
+
+
+
+
+
+
