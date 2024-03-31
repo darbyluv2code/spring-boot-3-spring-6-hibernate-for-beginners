a41b115b1023ff9b03f7c46bca01b6e2740d29fa
Chad Darby <darby@luv2code.com>
2024-03-31 06:10:10 -0400

    removed unused imports

--- 03-spring-boot-hibernate-jpa-crud/04-cruddemo-student-query-findByLastName/src/main/java/com/luv2code/cruddemo/CruddemoApplication.java
+++ 03-spring-boot-hibernate-jpa-crud/04-cruddemo-student-query-findByLastName/src/main/java/com/luv2code/cruddemo/CruddemoApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
-import java.sql.SQLOutput;
 import java.util.List;
 
 @SpringBootApplication
