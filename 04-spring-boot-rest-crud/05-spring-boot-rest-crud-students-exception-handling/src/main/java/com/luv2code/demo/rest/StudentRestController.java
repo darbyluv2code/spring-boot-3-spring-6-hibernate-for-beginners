efe74d9a4a17eb0ce31a239225a056616e6ba7e1
Chad Darby <darby@luv2code.com>
2024-03-31 06:12:35 -0400

    removed unused imports

--- 04-spring-boot-rest-crud/05-spring-boot-rest-crud-students-exception-handling/src/main/java/com/luv2code/demo/rest/StudentRestController.java
+++ 04-spring-boot-rest-crud/05-spring-boot-rest-crud-students-exception-handling/src/main/java/com/luv2code/demo/rest/StudentRestController.java
@@ -3,7 +3,6 @@ package com.luv2code.demo.rest;
 import com.luv2code.demo.entity.Student;
 import jakarta.annotation.PostConstruct;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
