e6ef21e46912d3f7c282e3f559bc5b5f2eb24bd3
darbyluv2code <darby@luv2code.com>
2025-02-28 22:50:32 -0500

    Update for HTTP PATCH method

---
+++ 04-spring-boot-rest-crud/13-spring-boot-rest-crud-employee-rest-controller-update-employee/src/main/java/com/luv2code/springboot/cruddemo/dao/EmployeeDAO.java
@@ -0,0 +1,25 @@
+package com.luv2code.springboot.cruddemo.dao;
+
+import com.luv2code.springboot.cruddemo.entity.Employee;
+
+import java.util.List;
+
+public interface EmployeeDAO {
+
+    List<Employee> findAll();
+
+    Employee findById(int theId);
+
+    Employee save(Employee theEmployee);
+
+}
+
+
+
+
+
+
+
+
+
+
