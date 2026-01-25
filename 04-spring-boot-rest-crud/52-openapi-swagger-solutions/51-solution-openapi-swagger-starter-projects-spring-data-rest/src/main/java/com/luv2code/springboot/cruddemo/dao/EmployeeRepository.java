e8e28ee12e278c9433fb55e15a9a5070105bb9a3
madhur verma <madhurciso01@gmail.com>
2026-01-25 18:56:56 +0530

    lrn

--- 04-spring-boot-rest-crud/52-openapi-swagger-solutions/51-solution-openapi-swagger-starter-projects-spring-data-rest/src/main/java/com/luv2code/springboot/cruddemo/dao/EmployeeRepository.java
+++ 04-spring-boot-rest-crud/52-openapi-swagger-solutions/51-solution-openapi-swagger-starter-projects-spring-data-rest/src/main/java/com/luv2code/springboot/cruddemo/dao/EmployeeRepository.java
@@ -2,7 +2,8 @@ package com.luv2code.springboot.cruddemo.dao;
 
 import com.luv2code.springboot.cruddemo.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
-
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+@RepositoryRestResource(path="employeeRepository")
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 
     // that's it ... no need to write any code LOL!
